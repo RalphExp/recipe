74ba48b1ed5c1516f60e5ca121bec09c841a3bf9
RalphExp <ralphexp@gmail.com>
2024-10-18 13:16:39 -0400

    add coverage test

--- script/test-api-server.sh
+++ script/test-api-server.sh
@@ -1,5 +1,13 @@
-(cd ../api; \
-JWT_SECRET=eUbP9shywUygMx7u \
-MONGO_URI="mongodb://admin:password@localhost:27017/test?authSource=admin" \
-MONGO_DATABASE=demo \
-go test -v *.go)
+export JWT_SECRET=eUbP9shywUygMx7u
+export  MONGO_URI="mongodb://admin:password@localhost:27017/test?authSource=admin" 
+export MONGO_DATABASE=demo
+
+PWD=$(pwd)
+cd ../api;
+go test -v *.go
+if [ $? == 0 ]; then
+  GIN_MODE=release go test -v -coverprofile=coverage.out ./...
+  go tool cover -html=coverage.out
+fi
+
+cd $PWD
